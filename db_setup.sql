844e29546231f8e562af1164c61fb0f960e815ca
martin <prepxc@gmail.com>
2019-07-30 21:02:36 -0500

    Updated .sql file to actually work

--- db_setup.sql
+++ db_setup.sql
@@ -1,3 +1,9 @@
-CREATE ROLE "test_user" WITH LOGIN PASSWORD 'testpass';
+-- to run 
+-- sudo su postgres
+-- psql
+-- CREATE DATABASE spots_db;
+-- \q
+-- pqsl -d spots_db -f db_setup.sql
 
-CREATE DATABASE "spots_db";
+create user test_user with encrypted password 'testpass';
+grant all privileges on database spots_db to test_user;
