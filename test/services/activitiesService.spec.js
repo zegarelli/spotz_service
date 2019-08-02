dc786fe63697e6ac538ae36416691c4551dafca4
martin <prepxc@gmail.com>
2019-08-01 21:42:45 -0500

    testing something more legit

--- test/services/activitiesService.spec.js
+++ test/services/activitiesService.spec.js
@@ -1,17 +1,30 @@
 /* global before, describe, it, expect */
 
+const dbConfig = require('../../knexfile')
+const knex = require('knex')(dbConfig)
 const { get, search } = require('../../services/activitiesService')
 
 describe('get', () => {
   it('is a function', () => {
+    const expected = 'function'
+    
     const result = typeof get
-    expect(result).toEqual('function')
+    expect(result).toEqual(expected)
   })
 })
 
 describe('search', () => {
   it('is a function', () => {
+    const expected = 'function'
+
     const result = typeof search
-    expect(result).toEqual('function')
+    expect(result).toEqual(expected)
+  })
+
+  it('returns an array', async () => {
+    const expected = 0
+
+    const result = await search({})
+    expect(result.length).toBeGreaterThan(expected)
   })
 })
