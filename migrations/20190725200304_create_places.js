b69173f727bbc219b0e313efc6098f8163b3830e
Martin Zegarelli <prepxc@gmail.com>
2019-08-09 21:16:50 -0500

    chore: add created/updated at/by columns to tables

--- migrations/20190725200304_create_places.js
+++ migrations/20190725200304_create_places.js
@@ -3,7 +3,10 @@ exports.up = async function (knex) {
   await knex.schema.createTable('places', table => {
     table.increments('id').primary()
     table.string('name')
-    table.integer('creator_id').references('users.id')
+    table.integer('created_by').references('users.id')
+    table.integer('updated_by').references('users.id')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
     table.json('extended_data')
   })
 }
