453ab3a85ef426be683444e5ccba7a9d305587f4
Martin Zegarelli <prepxc@gmail.com>
2019-08-09 21:50:41 -0500

    chore: expanded extended data for places

--- seeds/1_places.js
+++ seeds/1_places.js
@@ -12,7 +12,12 @@ exports.seed = function (knex) {
           updated_by: 3,
           created_at: moment().subtract(2, 'hours'),
           updated_at: moment().subtract(1, 'minute'),
-          extended_data: { description: "It's where you finish your night", opens: '1:30 AM', closes: '3:00 AM' }
+          extended_data: {
+            description: "It's where you finish your night",
+            opens: '1:30 AM',
+            closes: '3:00 AM',
+            imagePath: 'https://lh5.googleusercontent.com/p/AF1QipPhBburW6CsBl35dO-ygHZaDq-z8dyIoV_R1lHc=s387-k-no'
+          }
         },
         {
           name: 'Harpos',
@@ -23,7 +28,8 @@ exports.seed = function (knex) {
           extended_data: {
             description: 'All they serve is RBVs',
             opens: '7 PM',
-            closes: '1:00 AM'
+            closes: '1:00 AM',
+            imagePath: 'https://visitkcfiles.s3-us-west-2.amazonaws.com/37326-Harpos-01-1323.jpg'
           }
         },
         {
@@ -35,7 +41,8 @@ exports.seed = function (knex) {
           extended_data: {
             description: 'We have boobs',
             opens: '1:30 AM',
-            closes: '3:00 AM'
+            closes: '3:00 AM',
+            imagePath: 'https://ak.jogurucdn.com/media/image/p25/place-2018-02-5-11-43deaf18569ea64b04aaaf5cabebe0d8.jpg'
           }
         }
       ])
