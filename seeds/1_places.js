0d69361d9e3d24d7879ccd9923204c00da4e2653
Martin Zegarelli <prepxc@gmail.com>
2019-08-09 20:56:45 -0500

    feat: seed extended data for places

--- seeds/1_places.js
+++ seeds/1_places.js
@@ -5,9 +5,9 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('places').insert([
-        { name: 1, creator_id: 3 },
-        { name: 2, creator_id: 2 },
-        { name: 3, creator_id: 1 }
+        { name: 'Buzzard Beach', creator_id: 3, extended_data: { description: "It's where you finish your night", opens: '1:30 AM', closes: '3:00 AM' } },
+        { name: 'Harpos', creator_id: 2, extended_data: { description: 'All they serve is RBVs', opens: '7 PM', closes: '1:00 AM' } },
+        { name: 'Bazookas', creator_id: 1, extended_data: { description: 'We have boobs', opens: '1:30 AM', closes: '3:00 AM' } }
       ])
     })
 }
