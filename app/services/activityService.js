6b38400820f58306adf4b0bbbca0fb3b1a05ae6b
martin <martinzegarelli@gmail.com>
2020-04-04 17:01:24 -0500

    refactor: move activity service

---
+++ app/services/activityService.js
@@ -0,0 +1,15 @@
+const Activity = require('../models/Activity')
+
+async function search (name, creator, place) {
+  let query = await Activity.query()
+  query = name ? query.where({ name }) : query
+  query = creator ? query.where({ creator }) : query
+  query = place ? query.where({ place }) : query
+  //   query = after ? query.where('createdAt', '>=', after) : query
+  //   query = before ? query.where('createdAt', '<=', before) : query
+  return query
+}
+
+module.exports = {
+  search
+}
