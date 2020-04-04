7b2b7577aa2c0981d5b528d95ba04c278aa8721d
martin <martinzegarelli@gmail.com>
2020-04-04 17:00:19 -0500

    chore: handle errors, and remove redundant routes in activities

--- app/routes/activities.js
+++ app/routes/activities.js
@@ -5,7 +5,7 @@ const Activity = require('../models/Activity')
 const activityService = require('../services/activityService')
 
 /* GET activities listing. */
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   const name = req.query.name || null
   const creator = req.query.creator || null
   const place = req.query.place || null
@@ -14,23 +14,20 @@ router.get('/', async (req, res) => {
     const results = await activityService.search(name, creator, place)
     res.json(results)
   } catch (err) {
-    res.json(err)
+    err.metaData = {
+      ...err.metaData,
+      query: req.query
+    }
+    next(err)
   }
 })
 
-router.get('/all', async (req, res) => {
-  const activities = await Activity.query()
-  res.json(activities)
-})
-
-router.get('/:id', async (req, res) => {
-  const activities = await Activity.query().findById(req.params.id)
-  res.json(activities)
-})
-
-router.get('/:id/users', async (req, res) => {
-  const activities = await Activity.query().findById(req.params.id).eager('users')
-  res.json(activities)
+router.get('/:id', async (req, res, next) => {
+  try {
+    res.json(await Activity.query().findById(req.params.id))
+  } catch (err) {
+    next(err)
+  }
 })
 
 module.exports = router
