a9e2ed705aec1e72c675bd1cf6570e95966002fb
martin <prepxc@gmail.com>
2020-04-20 21:12:54 -0500

    chore: users/verify, userservice.ensureUser & migrate & seeds

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const router = express.Router()
-
+const userService = require('../services/userService')
 const User = require('../models/User')
 
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
   try {
     res.json(await User.query())
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/verify', async (req, res, next) => {
+  console.log('in verify route')
+  try {
+    if (req.body.id_token) {
+      const user = await userService.ensureUser(req.body.id_token)
+      res.json(user)
+    } else {
+      throw new Error('id_token not found in cookies')
+    }
   } catch (err) {
     next(err)
   }
