6752f2110bb7a0c27351252bdb017972b1546500
martin <martin.zegarelli@choosemylo.com>
2020-05-08 01:34:08 -0500

    chore: use static files rather than hosting

--- app/common/config.js
+++ app/common/config.js
@@ -1,7 +1,7 @@
 const siteConfig = {
-  jsMain: 'http://spotzreact.s3-website.us-east-2.amazonaws.com/static/js/main.7562726f.chunk.js',
-  jsMain2: 'http://spotzreact.s3-website.us-east-2.amazonaws.com/static/js/2.b34bd16c.chunk.js',
-  jsMain3: 'http://spotzreact.s3-website.us-east-2.amazonaws.com/static/js/runtime-main.35eb2b37.js',
+  jsMain: 'https://spotzstatic.s3.us-east-2.amazonaws.com/main.7562726f.chunk.js',
+  jsMain2: 'https://spotzstatic.s3.us-east-2.amazonaws.com/2.b34bd16c.chunk.js',
+  jsMain3: 'https://spotzstatic.s3.us-east-2.amazonaws.com/runtime-main.35eb2b37.js',
   styleSheet: '//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css',
   title: 'Spotz2'
 }
