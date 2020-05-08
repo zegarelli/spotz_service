2f31ac5cf53c50720d058b509fa68235a538d1b5
martin <martin.zegarelli@choosemylo.com>
2020-05-07 19:25:43 -0500

    chore: serve up ejs files

--- app/common/config.js
+++ app/common/config.js
@@ -1,6 +1,9 @@
 const siteConfig = {
-  jsMain: 'http://spotzreact.s3-website.us-east-2.amazonaws.com/',
-  title: 'Spotz'
+  jsMain: 'http://spotzreact.s3-website.us-east-2.amazonaws.com/static/js/main.d85c877f.chunk.js',
+  jsMain2: 'http://spotzreact.s3-website.us-east-2.amazonaws.com/static/js/2.b34bd16c.chunk.js',
+  jsMain3: 'http://spotzreact.s3-website.us-east-2.amazonaws.com/static/js/runtime-main.35eb2b37.js',
+  styleSheet: '//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css',
+  title: 'Spotz2'
 }
 
 const allowedOrigins = [
