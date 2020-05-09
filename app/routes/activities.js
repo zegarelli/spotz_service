569526c8950d88126f57b3803bf574a2a9de945d
martin <prepxc@gmail.com>
2020-05-09 16:45:42 -0500

    chore: routes are protected by proper scopes

--- app/routes/activities.js
+++ app/routes/activities.js
@@ -21,7 +21,7 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-router.post('/', guard.checkAny(['admin:manage']), async (req, res, next) => {
+router.post('/', guard.checkAny(['objects:create']), async (req, res, next) => {
   try {
     const result = await activityService.create(req.body, req.user.id)
     res.json(result)
@@ -30,7 +30,7 @@ router.post('/', guard.checkAny(['admin:manage']), async (req, res, next) => {
   }
 })
 
-router.put('/:id', guard.checkAny(['admin:manage']), async (req, res, next) => {
+router.put('/:id', guard.checkAny(['objects:edit']), async (req, res, next) => {
   try {
     const result = await activityService.update(req.params.id, req.body, req.user.id)
     res.json(result)
